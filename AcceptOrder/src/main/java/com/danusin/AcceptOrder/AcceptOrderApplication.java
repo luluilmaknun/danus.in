5393443e05718d6725a180dc7da85c5eb8814ada
Alsabila Shakina <alsabila.shakina@gmail.com>
2019-04-07 15:22:38 +0700

    Initiate all services and .gitignore

---
+++ AcceptOrder/src/main/java/com/danusin/AcceptOrder/AcceptOrderApplication.java
@@ -0,0 +1,13 @@
+package com.danusin.AcceptOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceptOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcceptOrderApplication.class, args);
+	}
+
+}
