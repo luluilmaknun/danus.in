03f7b9cd5dededf5f925426bfb707c468cf88d45
Alsabila Shakina <alsabila.shakina@gmail.com>
2019-04-13 19:09:56 +0700

    create controller

---
+++ LogInOut/src/main/java/com/danusin/LogInOut/controller/LogInPageController.java
@@ -0,0 +1,16 @@
+package com.danusin.LogInOut.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LogInPageController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String openDefaultPage() {
+        return "This is for Log In";
+    }
+
+}
