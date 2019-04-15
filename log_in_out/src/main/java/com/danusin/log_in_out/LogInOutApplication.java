a34a765b5e85025a0614cd2293ecdf4df9cae35c
Alsabila Shakina <alsabila.shakina@gmail.com>
2019-04-15 08:57:29 +0700

    try deploy

---
+++ log_in_out/src/main/java/com/danusin/log_in_out/LogInOutApplication.java
@@ -0,0 +1,13 @@
+package com.danusin.log_in_out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInOutApplication.class, args);
+	}
+
+}
